e8dc88afe273c5abd6af54d294a53ade9092d80c
David Johnson <djohnson.m@gmail.com>
2017-02-11 17:46:59 -0600

    Initial commit of new CI

--- stripe-http-streams/default.nix
+++ stripe-http-streams/default.nix
@@ -1,3 +1,20 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
-            nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./stripe-http-streams.nix
-           { }
+{ mkDerivation, aeson, base, bytestring, free, HsOpenSSL, hspec
+, http-streams, io-streams, stdenv, stripe-core, stripe-tests, text
+}:
+mkDerivation {
+  pname = "stripe-http-streams";
+  version = "2.1.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bytestring HsOpenSSL http-streams io-streams stripe-core
+    text
+  ];
+  testHaskellDepends = [
+    base free HsOpenSSL hspec http-streams stripe-core stripe-tests
+  ];
+  preCheck = ''
+   export STRIPEKEY="sk_test_igoYowTqR5IfovOKFKwigRmW"
+  '';
+  description = "Stripe API for Haskell - http-streams backend";
+  license = stdenv.lib.licenses.mit;
+}
