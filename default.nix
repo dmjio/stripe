17d02b7a0fe8a7af61796294bf4d8f09c54ca6fa
David Johnson <djohnson.m@gmail.com>
2017-02-13 00:18:42 -0600

    refactored nix override, added conditional haddocking

--- default.nix
+++ default.nix
@@ -1,34 +1,28 @@
-{ compiler ? "ghc802", profiling ? false }:
+{ compiler ? "ghc802", profiling ? false, haddock ? false }:
   let
    config = {
      packageOverrides = pkgs: {
-       haskell = pkgs.haskell // {
-         packages = pkgs.haskell.packages // {
-           ${compiler} = pkgs.haskell.packages.${compiler}.override {
-              overrides = self: super: rec {
-                mkDerivation = args: super.mkDerivation (args // {
-                  # Only run tests on stripe-http-streams
- 		  doCheck = args.pname == "stripe-http-streams";
-		  doHaddock = false;
-		  enableLibraryProfiling = profiling;
-		});
-                stripe-core = self.callPackage ./stripe-core { };
-                stripe-tests = self.callPackage ./stripe-tests { inherit stripe-core; };
-                stripe-http-streams = self.callPackage ./stripe-http-streams {
-                   inherit stripe-tests stripe-core;
-                };
-                stripe-haskell = self.callPackage ./stripe-haskell {
-                  inherit stripe-http-streams stripe-core;
-                };
-              };
+       haskell.packages.${compiler} = pkgs.haskell.packages.${compiler}.override {
+          overrides = self: super: rec {
+            mkDerivation = args: super.mkDerivation (args // {
+              # Only run tests on stripe-http-streams
+              doCheck = args.pname == "stripe-http-streams";
+              doHaddock = haddock;
+	      enableLibraryProfiling = profiling;
+	      });
+            stripe-core = self.callPackage ./stripe-core { };
+            stripe-tests = self.callPackage ./stripe-tests { inherit stripe-core; };
+            stripe-http-streams = self.callPackage ./stripe-http-streams {
+              inherit stripe-tests stripe-core;
+            };
+            stripe-haskell = self.callPackage ./stripe-haskell {
+              inherit stripe-http-streams stripe-core;
             };
           };
         };
       };
     };
-   pkgs = import <nixpkgs> { inherit config; };
-   lib = pkgs.haskell.lib;
-   in with pkgs.haskell.packages.${compiler}; {
+   in with (import <nixpkgs> { inherit config; }).haskell.packages.${compiler}; {
      inherit stripe-core stripe-tests stripe-haskell stripe-http-streams;
    }
 
