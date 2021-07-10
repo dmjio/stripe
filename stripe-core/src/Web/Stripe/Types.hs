5ef3c307922b3226611371a17869b3ca21fd715c
Libby Horacek <libby@daydrea.me>
2021-07-10 14:16:26 -0400

    Remove metadata field from ApplicationFee
    This was causing a JSON parse error.

--- stripe-core/src/Web/Stripe/Types.hs
+++ stripe-core/src/Web/Stripe/Types.hs
@@ -1409,7 +1409,6 @@ data ApplicationFee = ApplicationFee {
     , applicationFeeAccountId          :: Expandable AccountId
     , applicationFeeApplicationId      :: ApplicationId
     , applicationFeeChargeId           :: Expandable ChargeId
-    , applicationFeeMetaData           :: MetaData
 } deriving (Read, Show, Eq, Ord, Data, Typeable)
 
 ------------------------------------------------------------------------------
@@ -1445,7 +1444,6 @@ instance FromJSON ApplicationFee where
                       <*> o .: "account"
                       <*> (ApplicationId <$> o .: "application")
                       <*> o .: "charge"
-                      <*> o .: "metadata"
    parseJSON _ = mzero
 
 ------------------------------------------------------------------------------
